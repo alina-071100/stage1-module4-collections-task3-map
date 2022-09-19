5624b4d56f5328101fc4d8bb1ee60353f92f3cd5
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-09-19 16:19:30 +0400

    Map Interface

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc.collections.map;
 
 import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        return Pattern.compile(",|\\s|\\.")
+                .splitAsStream(sentence)
+                .map(String::toLowerCase)
+                .filter(s -> !s.isEmpty())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(x -> 1)));
+
     }
 }
